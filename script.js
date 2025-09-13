cda5a23da190512f4d9eaec096d001739173cfc0
Arpan Das <155471996+D-Arpan@users.noreply.github.com>
2025-09-13 22:57:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let form = document.querySelector("form");
+let nameInp = document.querySelector("#name");
+let emailInp = document.querySelector("#email");
+let passInp = document.querySelector("#password");
+let genderInp = document.querySelector("#gender");
+
+
+
+form.addEventListener("submit", function (dets) {
+    dets.preventDefault();
+
+    if (nameInp.value.length <= 2) {
+        document.querySelector("#alertName").style.display = "initial";
+    } else {
+        document.querySelector("#alertName").style.display = "none";
+    };
+
+
+
+    const emailPattern = /^(?!.*\.\.)([a-zA-Z0-9]+[a-zA-Z0-9._%+-]*)@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+$/;
+    if (!emailPattern.test(emailInp.value)) {
+        document.querySelector("#alertEmail").style.display = "initial";
+    } else {
+        document.querySelector("#alertEmail").style.display = "none";
+    };
+
+
+
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordPattern.test(passInp.value)) {
+        document.querySelector("#alertPass").style.display = "initial";
+    } else {
+        document.querySelector("#alertPass").style.display = "none";
+    };
+});
+
